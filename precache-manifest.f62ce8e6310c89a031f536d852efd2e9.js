ea75c4489b0e022ace04dfd1000ef18857d522f1
Jacob Tersander <you@example.com>
2020-10-16 23:43:05 +0200

    Updates

---
+++ precache-manifest.f62ce8e6310c89a031f536d852efd2e9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "44284782fd4ccb472414c879a8dc90fc",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "8eed12a71b1a844816e4",
+    "url": "/portfolio/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "7357af019cfbac6eb3cf",
+    "url": "/portfolio/static/js/2.9e7789ca.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/portfolio/static/js/2.9e7789ca.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8eed12a71b1a844816e4",
+    "url": "/portfolio/static/js/main.10a1b4a7.chunk.js"
+  },
+  {
+    "revision": "7a6cc99bd8f08f9fa6ec",
+    "url": "/portfolio/static/js/runtime-main.209c2d7e.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/portfolio/static/media/logo.5d5d9eef.svg"
+  }
+]);
